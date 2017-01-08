42494d3f1e06362160874bcf25ad671da0842a6c
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2017-01-07 22:36:05 -0600

    Add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#Install Bluethooth dependencies
+sudo pacman -S bluez bluez-utils
+
+#Install xboxdrv
+yaourt -S --noconfirm xboxdrv 
+
+#Copy xboxdrv configuration for OUYA Controller
+sudo cp ./conf/xboxdrv /etc/default/xboxdrv
+sudo cp ./conf/start.macro /etc/default/start.macro
+sudo cp ./conf/99-ouya.rules /etc/udev/rules.d/99-ouya.rules
